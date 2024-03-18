fe46f6cff9165f3fa8c2367bfd00e694b82e95ae
Lyrise <lyrise1984@gmail.com>
2024-03-19 08:11:03 +0900

    fix

--- update.sh
+++ update.sh
@@ -1,11 +1,20 @@
 #/bin/bash
 
+# download zip
 poetry run python update.py
 
-unzip -o -d bedrock_new bedrock.zip
+# clean up
+rm -rf bedrock_new/
+rm -rf bedrock_old/
+
+# unzip
+unzip -o bedrock.zip -d bedrock_new
 rm bedrock.zip
 
+# move files
 mv bedrock/worlds/ bedrock_new/
 mv bedrock/server.properties bedrock_new/
+
+# swap
 mv bedrock/ bedrock_old/
 mv bedrock_new/ bedrock/
